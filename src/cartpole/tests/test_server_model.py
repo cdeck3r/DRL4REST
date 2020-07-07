57c089b7ce37e1f2b52c7020ab1b624582d4e818
cdeck3r <cdecker@outlook.de>
2020-07-07 11:15:21 +0200

    Flaked test_server_model

--- src/cartpole/tests/test_server_model.py
+++ src/cartpole/tests/test_server_model.py
@@ -4,16 +4,20 @@ from deap import gp
 from cartpole.gprest.server_model import CartpoleServer
 import cartpole.gprest.server_model as sm
 
+
+# small test routine for the ServerModel
 class Test_ServerModel(unittest.TestCase):
-    # small test routine for the ServerModel
-      
+
     def setUp(self):
-        self.s=CartpoleServer
-        self.cps_crud_func = [func for func in dir(self.s) if callable(getattr(self.s, func)) 
-               and not func.startswith('__')
-               and not func.startswith('reset')]
+        self.s = CartpoleServer
+        self.cps_crud_func = [
+            func for func in dir(self.s)
+            if callable(getattr(self.s, func))
+            and not func.startswith('__')
+            and not func.startswith('reset')
+        ]
         self.s.reset()
-    
+
     def _crud_funcs(self, model_name):
         cps_model_func = [m for m in self.cps_crud_func if m.endswith(model_name)]
         pset = gp.PrimitiveSet("MAIN", 0)
@@ -29,15 +33,16 @@ class Test_ServerModel(unittest.TestCase):
         model_name = 'pole'
         self._crud_funcs(model_name)
 
-    # Assert there are the number of instances created 
+    # Assert there are the number of instances created
     # and they are all different.
     def test_instances(self):
-        num_instances = random.randint(1,100)
+        num_instances = random.randint(1, 100)
         self.s.n_instances(n=num_instances)
         all_inst = self.s._instances
 
         self.assertEqual(len(all_inst), num_instances)
         self.assertEqual(len(set(all_inst)), num_instances)
+
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test_ServerModel)
