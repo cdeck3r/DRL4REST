6b2a4f5a741ca87f3c5dcf6f941957454c858d0f
cdeck3r <cdecker@outlook.de>
2020-07-07 11:28:03 +0200

    Completed testcase, flaked

--- src/cartpole/tests/test_gp_testdefaultcontroller.py
+++ src/cartpole/tests/test_gp_testdefaultcontroller.py
@@ -1,6 +1,6 @@
 import unittest
 from cartpole.gprest.gp_test_default_controller import GP_TestDefaultController
-from cartpole.gprest.monkey_patching import MonkeyPatching
+from openapi_server.models import Cart
 
 
 class Test_GP_TestDefaultController(unittest.TestCase):
@@ -8,15 +8,19 @@ class Test_GP_TestDefaultController(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.gp_test=GP_TestDefaultController()
-        
+        cls.gp_test = GP_TestDefaultController()
+
     def setUp(self):
-        self.gp_test=Test_GP_TestDefaultController.gp_test
+        self.gp_test = Test_GP_TestDefaultController.gp_test
 
     def test_default(self):
         gp_cart = self.gp_test.test_cart_get()
+        gp_cart = Cart.from_dict(gp_cart.json)
+        self.assertIsInstance(gp_cart, Cart)
 
 
 if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test_GP_TestDefaultController)
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(
+        Test_GP_TestDefaultController
+    )
     unittest.TextTestRunner(verbosity=2).run(suite)
