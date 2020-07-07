6b478b0c2fc51befab57272ccf9bf64bf3264f7c
cdeck3r <cdecker@outlook.de>
2020-07-07 11:20:25 +0200

    Flaked conftest.py

--- src/cartpole/tests/conftest.py
+++ src/cartpole/tests/conftest.py
@@ -2,20 +2,23 @@
 # By convention this file shares common function, fixtures, hooks and others
 #
 
-# we need to add the path where OpenAPI's generated server stub is located 
+# we need to add the path where OpenAPI's generated server stub is located
 # e.g. DRL4REST/openapi/cartpole/python-flask
 
-import sys, os
+import sys
+import os
 
 here_path = os.path.dirname(os.path.abspath(__file__))
 
 cartpole_pkg = os.path.abspath(here_path + '/../../')
 # it must exist
 assert os.path.exists(cartpole_pkg)
-sys.path.append(cartpole_pkg) 
+sys.path.append(cartpole_pkg)
 
-cartpole_server_stub = os.path.abspath(here_path + '/../../../openapi/cartpole/python-flask')
+cartpole_server_stub = os.path.abspath(
+    here_path + '/../../../openapi/cartpole/python-flask'
+)
 # it must exist
 assert os.path.exists(cartpole_server_stub)
 # Finally, we add the generated server stub to PYTHONPATH
-sys.path.append( cartpole_server_stub ) 
+sys.path.append(cartpole_server_stub)
